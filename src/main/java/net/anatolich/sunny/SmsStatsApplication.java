58b7be87cbb7b1eddaef0fa8c6299ead7e6ed96a
Dmytro Kovalchuk <dimasmith@gmail.com>
2016-09-02 22:53:22 +0300

    Baseline spring boot application

---
+++ src/main/java/net/anatolich/sunny/SmsStatsApplication.java
@@ -0,0 +1,12 @@
+package net.anatolich.sunny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsStatsApplication.class, args);
+	}
+}
